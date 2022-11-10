ab81f79db39a46a2a48217df83edabe4555a831a
Michael R. Taylor <michael.taylor@gardnerdenver.com>
2022-11-10 15:34:00 -0600

    Added nvim-treesitter-textobjects submodule and config

--- init.lua
+++ init.lua
@@ -114,6 +114,18 @@ require('nvim-treesitter.configs').setup({
 	highlight = {
 		enable = true,
 	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				['af'] = '@function.outer',
+				['if'] = '@function.inner',
+				['ac'] = '@class.outer',
+				['ic'] = '@class.inner',
+			}
+		},
+	},
 	ensure_installed = treesitter_langs,
 })
 
