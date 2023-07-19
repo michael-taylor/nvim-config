227a2cfa053f8af2f67a2c47b21e76d6e9b1d8d8
Michael R. Taylor <michael@atomic.city>
2023-07-18 22:41:03 -0500

    Copied 'h' and 'l' key behavior in 'neo-tree' from AstroNvim

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function()
+      return {
+        commands = {
+          parent_or_close = function(state)
+            local node = state.tree:get_node()
+            if (node.type == "directory" or node:has_children()) and node:is_expanded() then
+              state.commands.toggle_node(state)
+            else
+              require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+            end
+          end,
+          child_or_open = function(state)
+            local node = state.tree:get_node()
+            if node.type == "directory" or node:has_children() then
+              if not node:is_expanded() then -- if unexpanded, expand
+                state.commands.toggle_node(state)
+              else -- if expanded and has children, seleect the next child
+                require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+              end
+            else -- if not a directory just open it
+              state.commands.open(state)
+            end
+          end,
+        },
+        window = {
+          mappings = {
+            h = "parent_or_close",
+            l = "child_or_open",
+          },
+        },
+      }
+    end,
+  },
+}
